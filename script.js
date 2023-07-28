99cd48d585fa84d0e98402185c6ae10ed632bd16
Jeni B <78270336+suzyq2nice@users.noreply.github.com>
2023-07-28 09:20:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const calculate = () => {
-  let p = Number(document.getElementById("principal").value;
-  let r = Number(document.getElementById("rate").value;
-  let t = Number(document.getElementById("time").value;
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p - simpleInterest;
+    let p = Number(document.getElementById("principal").value);
+    let r = Number(document.getElementById("rate").value);
+    let t = Number(document.getElementById("time").value);
+    let simpleInterest = calculateSimpleInterest(p, r, t);
+    let amount = calculateTotalPayableAmount(p, simpleInterest);
+  
+    let result = document.getElementById("result");
+  
+    result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+    <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+    <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+  };
 
-  let result = document.getElementById("result");
-
-  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
-  <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
-  <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
-};
+  if (typeof module !== 'undefined')
+  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
